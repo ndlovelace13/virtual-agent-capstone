eb672814c5d5778d3393fad71757ff1be8e7d714
Kamden Watterberg (Student) <kwatterb@sundevils.asu.edu>
2024-11-02 16:17:22 -0700

    Initial Welcome Message When Entering AR Mode

--- VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.40.33813
-#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
+// Compiler: 14.34.31942
+#include "C:/Users/kwatterb/Desktop/UnrealEngineProject/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
--- VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "C:/Users/kwatterb/Desktop/UnrealEngineProject/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
