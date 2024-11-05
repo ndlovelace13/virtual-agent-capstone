22f173cf3cbb83d55496acb33de68fb005f5bfea
Kamden Watterberg (Student) <kwatterb@sundevils.asu.edu>
2024-11-05 13:13:46 -0700

    Implemented TTS functionality, ability to type and hear voice and added a welcome message when entering AR mode

--- VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.40.33813
-#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
+#include "C:/Users/kwatterb/Desktop/UEProject/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
