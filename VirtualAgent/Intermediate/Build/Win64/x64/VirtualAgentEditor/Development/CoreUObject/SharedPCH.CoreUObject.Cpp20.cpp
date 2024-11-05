65ce423a7bd36645515202bf4a1624180a9398ee
Noah Lovelace <ndlovelace13@gmail.com>
2024-11-05 16:24:39 -0700

    Revert "Removed .bin file"
    This reverts commit 73598ada771ab559d7bceb4b0257d13906ade26c.

---
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.40.33813
+#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
---
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
