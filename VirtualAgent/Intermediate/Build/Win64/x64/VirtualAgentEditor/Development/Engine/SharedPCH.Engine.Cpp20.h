f3af30ed9a0854d3cc88b23697a341cb1259117b
Kamden Watterberg <kwatterb@asu.edu>
2025-02-09 22:54:50 -0700

    Allows user to move around the mini-map. Also included an extended version of minimap1

--- VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "C:/Users/watte/Desktop/CapstoneProject/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
--- VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
+++ VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.40.33813
-#include "C:/Users/jbsam/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
+// Compiler: 14.41.34120
+#include "C:/Users/watte/Desktop/CapstoneProject/virtual-agent-capstone/VirtualAgent/Intermediate/Build/Win64/x64/VirtualAgentEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
